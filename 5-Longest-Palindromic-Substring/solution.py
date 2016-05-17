0f11e9cd956a84596979fbbdbf6a25a5fadc401e
MBAII <baimingze369@gmail.com>
2016-05-17 15:58:14 -0400

    [5. Longest Palindromic Substring][Accepted]committed by LeetCode Extension

--- 5-Longest-Palindromic-Substring/solution.py
+++ 5-Longest-Palindromic-Substring/solution.py
@@ -1,41 +1,25 @@
 class Solution(object):
     def longestPalindrome(self, s):
-        longest_str = ""
         
-        for i in range(len(s)):
-            sub1 = self.expand1(s, i)
-            sub2 = self.expand2(s, i)
-            if len(sub1) > len(longest_str):
-                longest_str = sub1
-            if len(sub2) > len(longest_str):
-                longest_str = sub2
-        return  longest_str       
-            
-            
-            
-    def expand1(self, s,i):
-        sub = s[i]
-        l = i - 1
-        r = i + 1
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            sub = s[l] + sub + s[r]
-            l -= 1
-            r += 1
-        return sub
-        
-    def expand2(self, s,i):
-        l = i - 1
-        r = i + 2
-        if l < 0 or r >= len(s):
-            return ""
-        sub = s[i] + s[i + 1]
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            sub = s[l] + sub + s[r]
-            l -= 1
-            r += 1
-        return sub
         """
         :type s: str
         :rtype: str
         """
+        start = 0
+        end = 0
+        for i in range(len(s)):
+            len1 = self.expand(s,i,i)
+            len2 = self.expand(s,i,i+1)
+            len_max = max(len1,len2)
+            if len_max > end - start:
+                start = i - (len_max - 1) / 2
+                end = i + len_max / 2
+        return s[start:end+1]
         
+        
+    
+    def expand(self,s,l,r):
+        while l >= 0 and r < len(s) and s[l]==s[r]:
+            l -= 1
+            r += 1
+        return r - l - 1
