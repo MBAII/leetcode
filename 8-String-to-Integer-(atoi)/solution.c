e34753aaf18507ccfaa90e908ef51978601015e5
MBAII <baimingze369@gmail.com>
2016-05-18 22:46:22 -0400

    [8. String to Integer (atoi)][Compile Error]committed by LeetCode Extension

--- 8-String-to-Integer-(atoi)/solution.c
+++ 8-String-to-Integer-(atoi)/solution.c
@@ -1,7 +1,47 @@
-class Solution(object):
-    def myAtoi(self, str):
-        """
-        :type str: str
-        :rtype: int
-        """
+int myAtoi(char* str) {
+    // printf("INT_MAX: %d",INT_MAX + 1);
+    int l = strlen(str);
+    int result = 0;
+    int sign = 1;
+    int i = 0;
+    
+    while (i < l){
+        if (str[i] == ' '){
+            i++;
+            continue;
+        }
+        else{
+            break;
+        }
+    }
+    if (str[i] == '-'){
+        sign = -1;
+        i++;
+    }
+    else if(str[i] == '+'){
+        i++;
+    }
         
+    while (i < l){
+        if ((str[i] >= '0') && (str[i] <= '9')){
+            if (((sign == 1) && (result > INT_MAX / 10)) || ((sign == 1) && (result == INT_MAX / 10) && str[i] > '7')){
+                return INT_MAX
+            }
+            else if (((sign == -1) && (result > INT_MAX / 10)) || ((sign == -1) && (result == INT_MAX / 10) && str[i] > '8')){
+                return INT_MIN 
+            }
+            
+            result = result * 10 + str[i] - '0';
+        }
+        else{
+            i = l;
+        }
+        i++;
+    }
+    
+    if (sign == -1){
+        result *= -1;
+    }
+    return result;
+    
+}
